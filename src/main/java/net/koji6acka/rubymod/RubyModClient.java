d3c0bc7de81efefab54099c4714a3f8e948b8a8b
KoJI6ackaman <sanzhar697@gmail.com>
2024-04-12 18:49:16 +0500

    Initial commit

---
+++ src/main/java/net/koji6acka/rubymod/RubyModClient.java
@@ -0,0 +1,10 @@
+package net.koji6acka.rubymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
